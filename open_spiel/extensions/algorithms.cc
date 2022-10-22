45b831889b3383c694a26dfc02c327c8da46455f
JonathanLehner <lehnerj@student.ethz.ch>
2022-10-22 19:07:40 +0200

    Update algorithms.cc

--- open_spiel/extensions/algorithms.cc
+++ open_spiel/extensions/algorithms.cc
@@ -480,9 +480,10 @@ void loadBuckets(const std::string& lutPath) {
   
 // Load file to a cpp map
 void loadTurnPerFlopBuckets(const std::string& lutPath) {
-  printf("\n[algorithms] loading per-flop turn buckets..\t");
+  printf("\n[algorithms] loading per-flop turn buckets from ..\t");
+  printf(lutPath + "/turn_per_flop_emd.txt");
   fflush(stdout);
-  readDeepDictionaryFromJson(lutPath + "/turn_per_flop.txt", turnBucketPerFlop);
+  readDeepDictionaryFromJson(lutPath + "/turn_per_flop_emd.txt", turnBucketPerFlop);
   printf("DONE!\n");
 }
 
